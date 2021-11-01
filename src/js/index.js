cd6f6fb6327b7b17095bbb7db9f5569f7881c9aa
Anzhela Schults <anzhela.schults@gmail.com>
2021-11-01 12:23:43 +0100

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+
+const foo = require('./two')
+const greeting = require("./greeting")
+console.log ('Hello world')
+console.log("I am developer")
+
+require('./one')
+
+foo()
+greeting ("Ivan")
+greeting ("Marya")
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    mode: 'development'
+}
